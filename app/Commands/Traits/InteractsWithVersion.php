5d03177afbb38e849de436cb14e7d30582c5a800
Ayax Córdova <ayax.cordova@aydev.mx>
2025-02-16 23:48:19 -0600

    Fix code style issues

--- app/Commands/Traits/InteractsWithVersion.php
+++ app/Commands/Traits/InteractsWithVersion.php
@@ -11,8 +11,6 @@ trait InteractsWithVersion
      * The semver pattern.
      *
      * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-     *
-     * @var string
      */
     protected string $semverPattern = '/^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/';
 
