a969c7bdfe35a413df60cf5f66baf75c1411496b
Ayax Córdova <ayax.cordova@aydev.mx>
2025-02-18 16:26:05 -0600

    Refactor bootPackageTraits method for improved readability and efficiency

--- app/Commands/Traits/WithPackageTraitsBootstrap.php
+++ app/Commands/Traits/WithPackageTraitsBootstrap.php
@@ -10,74 +10,51 @@ trait WithPackageTraitsBootstrap
 {
     protected function bootPackageTraits(): void
     {
-        $default = [];
-        $beginning = [];
-        $middle = [];
-        $end = [];
+        $default = $beginning = $middle = $end = [];
 
         $classes = collect(class_uses_recursive($this));
 
-        tap(
-            $classes,
-            function ($traits) use (&$default, &$beginning, &$middle, &$end) {
-                $traits->each(function (string $trait) use (&$default, &$beginning, &$middle, &$end): void {
-                    try {
-                        $reflected = new \ReflectionMethod($trait, 'bootPackage'.class_basename($trait));
-                    } catch (\ReflectionException) {
-                        return;
-                    }
-
-                    $attributes = $reflected->getAttributes(Attributes\Order::class);
-
-                    if (empty($attributes)) {
-                        $default[] = $trait;
-
-                        return;
-                    }
-
-                    $attribute = $attributes[0]->newInstance();
-
-                    if ($attribute->getOrder() === -1) {
-                        $default[] = $trait;
-
-                        return;
-                    }
-
-                    if ($attribute->getOrder() === Attributes\Order::FIRST) {
-                        $beginning[] = $trait;
-
-                        return;
-                    }
-
-                    if ($attribute->getOrder() === Attributes\Order::LAST) {
-                        $end[] = $trait;
-
-                        return;
-                    }
-
-                    if (isset($middle[$attribute->getOrder()])) {
-                        $middle[$attribute->getOrder()][] = $trait;
-
-                        return;
-                    }
-
-                    $middle[] = $trait;
-                });
+        $classes->each(function (string $trait) use (&$default, &$beginning, &$middle, &$end): void {
+            try {
+                $reflected = new \ReflectionMethod($trait, 'bootPackage'.class_basename($trait));
+            } catch (\ReflectionException) {
+                return;
             }
-        );
+
+            $attributes = $reflected->getAttributes(Attributes\Order::class);
+
+            if (empty($attributes)) {
+                $default[] = $trait;
+
+                return;
+            }
+
+            $attribute = $attributes[0]->newInstance();
+
+            $order = $attribute->getOrder();
+
+            if ($order === -1) {
+                $default[] = $trait;
+            } elseif ($order === Attributes\Order::FIRST) {
+                $beginning[] = $trait;
+            } elseif ($order === Attributes\Order::LAST) {
+                $end[] = $trait;
+            } else {
+                $middle[$order][] = $trait;
+            }
+        });
 
         collect([
             ...$beginning,
             ...Arr::flatten($middle),
             ...$default,
             ...$end,
-        ])->each(fn (string $trait) => $this->bootPackageTraitUsing()($trait));
+        ])
+            ->each(fn (string $trait) => $this->bootPackageTraitUsing()($trait));
     }
 
     protected function bootPackageTraitUsing(?\Closure $using = null): \Closure
     {
-        return $using instanceof \Closure ? $using : function (string $trait) {
-            $this->{'bootPackage'.class_basename($trait)}();
-        };
+        return $using ?? fn (string $trait) => $this->{'bootPackage'.class_basename($trait)}();
     }
 }
