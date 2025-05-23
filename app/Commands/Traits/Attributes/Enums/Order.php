3675b7e07ca679a782907c7b36359a7cf8f4c80a
asciito <ayax.cordova@aydev.mx>
2025-05-22 18:07:56 -0600

    Remove unnecessary code
    Signed-off-by: asciito <ayax.cordova@aydev.mx>

--- app/Commands/Traits/Attributes/Enums/Order.php
+++ app/Commands/Traits/Attributes/Enums/Order.php
@@ -9,13 +9,4 @@ enum Order: int
     case FIRST = 1;
     case LAST = -1;
     case DEFAULT = 0;
-
-    public function getOrder(): int
-    {
-        return match ($this) {
-            self::FIRST => 1,
-            self::LAST => -1,
-            default => 0,
-        };
-    }
 }
