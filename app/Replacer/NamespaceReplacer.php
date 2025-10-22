cf55d8a1c94a357c538469d305d5296ad98369c6
asciito <ayax.cordova@aydev.mx>
2025-10-21 21:32:36 -0600

    Fix code style issues

--- app/Replacer/NamespaceReplacer.php
+++ app/Replacer/NamespaceReplacer.php
@@ -29,7 +29,6 @@ class NamespaceReplacer
                         default => explode('/', $replacement),
                     };
 
-
                     return implode($separator, [$modifier($vendor), $modifier($package)]);
                 };
             });
