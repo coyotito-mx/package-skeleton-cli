68c98da0ec1ec968064130abb1e984cb8d2d4a41
asciito <ayax.cordova@aydev.mx>
2025-10-29 22:09:28 -0600

    Add `author` missing tests
    This commit also refactors the vendor/package retrieval to not ask for it if namespace was already defined

--- app/Commands/Concerns/InteractsWithNamespace.php
+++ app/Commands/Concerns/InteractsWithNamespace.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Commands\Concerns;
 
 use App\Replacer;
+use Closure;
 use Illuminate\Support\Str;
+use RuntimeException;
 use Symfony\Component\Console\Input\InputOption;
+use function Laravel\Prompts\text;
 
 trait InteractsWithNamespace
 {
@@ -19,9 +22,28 @@ trait InteractsWithNamespace
             Replacer\NamespaceReplacer::class => fn (): string => $this->getPackageNamespace(),
         ]);
 
+        $missing = function (string $message, string $key): string|Closure {
+            return function () use ($message, $key) {
+                if ($this->option('namespace')) {
+                    $matches = [];
+                    $namespace = $this->getPackageNamespace();
+
+                    preg_match('/^(?<vendor>\S+)\/(?<package>\S+)$/', $namespace, $matches);
+
+                    if (empty($matches)) {
+                        throw new RuntimeException("The provided namespace [$namespace] does not match <vendor>/<package> format");
+                    }
+
+                    return $matches[$key];
+                } else {
+                    return text($message);
+                }
+            };
+        };
+
         $this
-            ->addPromptRequiredArgument('vendor', 'Vendor name', 'What is the vendor name?')
-            ->addPromptRequiredArgument('package', 'Package name', 'What is the package name?')
+            ->addPromptRequiredArgument('vendor', 'Vendor name', $missing('What is the vendor name?', 'vendor'))
+            ->addPromptRequiredArgument('package', 'Package name', $missing('What is the package name?', 'package'))
             ->addOption('namespace', null, InputOption::VALUE_OPTIONAL, 'The namespace of the package');
     }
 
---
+++ tests/Feature/Commands/Concerns/InteractsWithAuthor.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Commands\Concerns\InteractsWithAuthor;
+use App\Commands\Concerns\InteractsWithNamespace;
+
+dataset('author', [
+    'mini',
+    'jane',
+    'alice',
+    'john',
+    'jane',
+]);
+
+it('replace author', function (string $author) {
+    testingReplacersInCommand('{{author}} Doe', InteractsWithAuthor::class);
+
+    $this->artisan('demo', ['--author' => $author])
+        ->expectsOutputToContain(ucfirst($author).' Doe')
+        ->assertSuccessful();
+})->with('author');
+
+it('replace author using namespace', function (string $author) {
+    testingReplacersInCommand('{{author}} Doe', InteractsWithAuthor::class, InteractsWithNamespace::class);
+
+    $this->artisan('demo', ['--namespace' => "$author/package"])
+        ->expectsOutputToContain(ucfirst($author).' Doe')
+        ->assertSuccessful();
+})->with('author');
