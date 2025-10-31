b7b8637d0f4dc038c99649898bba7a325ac8d8bb
asciito <ayax.cordova@aydev.mx>
2025-10-30 21:42:50 -0600

    Add tests for license

---
+++ tests/Feature/Commands/Concerns/InteractsWithLicense.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Commands\Concerns;
+
+use App\Commands\Concerns\InteractsWithLicense;
+
+it('replace license', function () {
+    testingReplacersInCommand(<<<'JSON'
+    {
+      "name": "vendor/package",
+      "description": "lorem ipsum dolor it sit amet",
+      "license": "{{license}}",
+      "require": {
+        "vendor/acme": "^1.0.0",
+        "vendor/action": "^2.0.0"
+      }
+    }
+    JSON, InteractsWithLicense::class);
+
+    $this->artisan('demo')
+        ->expectsOutput(<<<JSON
+        {
+          "name": "vendor/package",
+          "description": "lorem ipsum dolor it sit amet",
+          "license": "MIT",
+          "require": {
+            "vendor/acme": "^1.0.0",
+            "vendor/action": "^2.0.0"
+          }
+        }
+        JSON)
+        ->assertSuccessful();
+});
