be3201f489fc1dd1c3ae2f6ff842711c922c42b1
asciito <ayax.cordova@aydev.mx>
2025-12-05 00:17:14 -0600

    Start fresh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Composer;
-use App\Git;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,12 +13,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function register(): void
     {
-        $this->app->bind('composer', function ($app) {
-            return new Composer($app['files'], getcwd());
-        });
-
-        $this->app->bind('git', function () {
-            return new Git(getcwd());
-        });
+        //
     }
 }
