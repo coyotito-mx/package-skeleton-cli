21537cfa64b51b37669434e9ba6850a1ac1ff2ad
asciito <ayax.cordova@aydev.mx>
2025-12-06 16:02:37 -0600

    Add Author and Vendor replacers

---
+++ app/Replacers/AuthorReplacer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Replacers;
+
+class AuthorReplacer extends Builder
+{
+    protected static string $placeholder = 'author';
+}
---
+++ app/Replacers/VendorReplacer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Replacers;
+
+class VendorReplacer extends Builder
+{
+    protected static string $placeholder = 'vendor';
+}
