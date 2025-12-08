150df4692c5aeea806d5d30402f4a570e3f012cb
asciito <ayax.cordova@aydev.mx>
2025-12-08 13:35:28 -0600

    Add package 'type' replacer

---
+++ app/Replacers/TypeReplacer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Replacers;
+
+use App\Replacer;
+use App\Replacers\Exceptions\InvalidPackageType;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
+
+class TypeReplacer extends Builder
+{
+    protected static string $placeholder = 'type';
+
+    public function __construct(string $replacement)
+    {
+        $replacement = Str::slug($replacement);
+
+        InvalidPackageType::validate($replacement);
+
+        parent::__construct($replacement);
+    }
+
+    protected function configure(Replacer $replacer): void
+    {
+        $replacer
+            ->normalizeReplacementUsing(fn (Stringable $replacement): Stringable => $replacement->slug())
+            ->onlyWith(null);
+
+        parent::configure($replacer);
+    }
+}
