8d45569aab0de2198a3d16a9107a50496c83438d
asciito <ayax.cordova@aydev.mx>
2026-02-23 15:21:20 -0600

    Add ComposerFake class and implement fake method in Composer facade

--- app/Facades/Composer.php
+++ app/Facades/Composer.php
@@ -6,6 +6,15 @@ use Illuminate\Support\Facades\Facade;
 
 class Composer extends Facade
 {
+    public static function fake(): \App\DependencyManagers\ComposerFake
+    {
+        $fake = new \App\DependencyManagers\ComposerFake();
+
+        static::swap($fake);
+
+        return $fake;
+    }
+
     protected static function getFacadeAccessor(): string
     {
         return 'composer';
