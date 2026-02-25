49ddd197153f5bf5710f72e91769644b56bbbbd2
asciito <ayax.cordova@aydev.mx>
2026-02-24 21:10:32 -0600

    Enhance DescriptionReplacer with excluded modifiers and normalization logic

--- app/Replacers/DescriptionReplacer.php
+++ app/Replacers/DescriptionReplacer.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Replacers;
 
+use App\Replacer;
+use Illuminate\Support\Stringable;
+
 /**
  * Replacer for `description` placeholders
  *
@@ -12,4 +15,14 @@ namespace App\Replacers;
 class DescriptionReplacer extends Builder
 {
     protected static string $placeholder = 'description';
+
+    public function getExcludedModifiers(): array
+    {
+        return ['acronym', 'slug', 'snake', 'camel', 'pascal'];
+    }
+
+    protected function configure(): Replacer
+    {
+        return parent::configure()->normalizeReplacementUsing(fn (Stringable $replacement) => $replacement->ucfirst());
+    }
 }
