7bd90b781631e32ddf217eeafcb112a41fbb2fd2
asciito <ayax.cordova@aydev.mx>
2026-03-14 18:33:57 -0600

    Add tag to remove content and related tests

---
+++ app/TagRemoval.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Placeholders\Exceptions\PlaceholderNotFound;
+use App\Placeholders\PlaceholderBuilder;
+use Illuminate\Support\Str;
+
+/**
+ * PlaceholderB Builder class
+ */
+class TagRemoval extends Replacer
+{
+
+    /**
+     * Get the placeholder pattern
+     */
+    protected function getPattern(): string
+    {
+        return '/<remove>(?:[\s\S]*?)<\/remove>\s*/';
+    }
+
+    /**
+     * Replace the placeholders in the give $content
+     */
+    public function replace(string $content): string
+    {
+        return Str::replaceMatches($this->getPattern(), '', $content);
+    }
+}
---
+++ tests/Feature/TagRemovalTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+use App\TagRemoval;
+
+it('replace tag', function () {
+    expect(new TagRemoval)->replace('Hello <remove>World</remove>')->toBe('Hello ');
+});
+
+it('replace tag with a lot of text', function () {
+    expect(new TagRemoval)
+        ->replace(<<<TXT
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sem libero, rutrum ut congue quis, cursus
+        quis lectus. Phasellus at laoreet purus. Morbi sagittis ante eget varius tristique. Etiam tempor ac
+        lacus in congue.
+        
+        <remove>
+        Ut lobortis eros a ipsum varius, eget tristique risus laoreet. Vestibulum ultricies augue ligula, vitae
+        imperdiet urna tempus non. Ut ut vulputate est. Suspendisse potenti. Donec id imperdiet nisi.
+        Fusce sit amet quam et justo volutpat tincidunt. Praesent mattis lobortis nunc nec ultrices.
+        Nulla non lobortis enim, ac imperdiet sem.
+        
+        Morbi posuere non lacus quis aliquam. Sed eget ni sisuscipit, sollicitudin lorem ut, rhoncus eros.
+        Vestibulum arcu velit, congue quis est vitae, iaculis euismod lorem.
+
+        Nam et convallis nisi. Morbi sem enim, aliquet non justo et, scelerisque maximus enim
+        </remove>
+        
+        Ut ut erat felis. Phasellus pharetra mauris at lacus porttitor consequat. Nullam dapibus risus at
+        scelerisque porttitor. <remove>Sed finibus, mi at dignissim scelerisque, lacus nulla ullamcorper urna, sit
+        amet blandit purus arcu a erat. Praesent sit amet facilisis magna.</remove>
+
+        Mauris laoreet, ex ut elementum iaculis, sem massa vestibulum urna, quis efficitur purus sem a dui. Aenean
+        eget nisl eu enim gravida auctor et eu dolor.
+        
+        <remove>Aliquam non urna ac dui blandit ultricies ut nec purus. Nunc condimentum nisi nulla, scelerisque
+        venenatis lorem finibus quis.</remove> Donec ac rutrum felis, non accumsan justo. Ut rutrum tortor
+        sed felis ultrices ultricies. Proin eu vehicula sapien.
+        TXT)
+        ->toBe(<<<TOBE
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sem libero, rutrum ut congue quis, cursus
+        quis lectus. Phasellus at laoreet purus. Morbi sagittis ante eget varius tristique. Etiam tempor ac
+        lacus in congue.
+        
+        Ut ut erat felis. Phasellus pharetra mauris at lacus porttitor consequat. Nullam dapibus risus at
+        scelerisque porttitor. Mauris laoreet, ex ut elementum iaculis, sem massa vestibulum urna, quis efficitur purus sem a dui. Aenean
+        eget nisl eu enim gravida auctor et eu dolor.
+        
+        Donec ac rutrum felis, non accumsan justo. Ut rutrum tortor
+        sed felis ultrices ultricies. Proin eu vehicula sapien.
+        TOBE);
+});
